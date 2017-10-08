bf2f9ee3b32e0f4133931cb9c98090ed3794e003
deepak <deepakvkashya1995@gmail.com>
2017-10-08 11:17:32 +0000

    WebSocket connection fixed

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -17,7 +17,7 @@ app.controller('chatController', function ($scope, $sce) {
     $scope.messages=[];
 
 
-var  exampleSocket =  new  WebSocket("wss://swiftcode-siri.herokuapp.com//chatSocket");
+var  exampleSocket =  new  WebSocket("wss://swiftcode-siri.herokuapp.com/chatSocket");
     exampleSocket.onmessage  =   function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date()
